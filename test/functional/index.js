56133f3bcdac83129ef77b8caf290f75aa4bac0f
Steve Laing <steve.laing@gmail.com>
2013-03-19 21:14:41 +0000

    Rewire the test
    Functional tests can now access models and factory data.

--- test/functional/index.js
+++ test/functional/index.js
@@ -1,15 +1,35 @@
-var helper = require('../test-helper'),
-    app = helper.app,
-    http = helper.http;
+var helper          = require('../test-helper'),
+    assert          = helper.assert,
+    app             = helper.app,
+    Browser         = helper.zombie,
+    Content         = helper.Content,
+    Factory         = helper.Factory,
+    http            = helper.http,
+    mongoose        = helper.mongoose,
+    browser         = new Browser(),
+    databaseCleaner = new DatabaseCleaner('mongodb');
 
-describe('homepage', function() {
-  before(function() {
-    this.server = http.createServer(app).listen(process.env.PORT);
+describe('homepage', function(){
+
+  before(function(){
+    http.createServer(app).listen(process.env.PORT);
+    Factory.create('content',function(){});
+  });
+  
+  it('should show some headings', function(done){
+    browser.visit("http://localhost:" + process.env.PORT + "/", { debug : true }, 
+      function () {
+        // Uncomment to see the doc source.
+        // console.log(browser.document.innerHTML)
+        assert.ok(browser.success);
+        assert.equal("Testing!", browser.text("h1"))
+        assert.equal("Some test content", browser.text("h3"));
+        done();
+      }
+    );
   });
 
-  it('should show intro text');
-  
-  after(function(done) {
-    this.server.close(done);
+  after(function(){
+    databaseCleaner.clean(mongoose.connections[0].db, function() {});
   });
 });
