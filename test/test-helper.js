2f177140dfa8ebc1e4be3ec4852a24cd9868a0fc
Steve Laing <steve.laing@gmail.com>
2013-03-19 21:13:04 +0000

    Add factory and exports

--- test/test-helper.js
+++ test/test-helper.js
@@ -1,8 +1,23 @@
 process.env.NODE_ENV = 'test'
 process.env.PORT = 3001
+process.env.MONGODB_CONN = "mongodb://localhost:27017/lsl_test"
+
+var  Content = require('../models/content')
+  ,  Factory = require('factory-lady');
+
+Factory.define('content', Content, {
+  title    : "Testing!", 
+  path     : '/',
+  body     : '### Some test content'
+});
 
 module.exports = {
-  app: require('../app'),
-  browser: require('zombie'),
-  http: require('http')
-};
+  app             : require('../app'),
+  assert          : require('assert'),
+  Content         : Content,
+  DatabaseCleaner : require('database-cleaner'),
+  Factory         : Factory,
+  http            : require('http'),
+  mongoose        : require('mongoose'),
+  zombie          : require('zombie')
+}
