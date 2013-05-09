9817a8ff27e22436d72d91475c5c36a843fbda07
Steve Laing <steve.laing@gmail.com>
2013-05-09 08:56:34 +0100

    Remove zombie, add request

--- test/test-helper.js
+++ test/test-helper.js
@@ -4,11 +4,11 @@ process.env.PORT = 3001
 process.env.MONGOHQ_URL = "mongodb://localhost:27017/lsl_test"
 
 var  app = require('../app')
-  ,  Browser = require('zombie')
   ,  Content = require('../models/content')
   ,  Factory = require('factory-lady')
   ,  http    = require('http')
-  ,  mongoose = require('mongoose');
+  ,  mongoose = require('mongoose')
+  ,  request = require('request');
 
 /* Factories */
 Factory.define('content', Content, {
@@ -20,13 +20,11 @@ Factory.define('content', Content, {
 /* Setup */
 before(function(done) {
   this.server = http.createServer(app).listen(process.env.PORT);
-  this.browser = new Browser({ debug:true, site: "http://localhost:"+process.env.PORT });
   this.databaseCleaner = new DatabaseCleaner('mongodb');
   done();
 });
 /* Teardown */
 after(function(done) {
-  //this.browser.close();
   this.databaseCleaner.clean(mongoose.connections[0].db,done);
   this.server.close(done);
 });
@@ -34,11 +32,11 @@ after(function(done) {
 /* Exports */
 module.exports = {
   app             : app,
-  Browser         : Browser,
   Content         : Content,
   DatabaseCleaner : require('database-cleaner'),
   Factory         : Factory,
   http            : http,
   mongoose        : mongoose,
+  request         : request,
   should          : require('should')
 }
