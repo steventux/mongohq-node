bd732f6b538f59dc8ca1d3c0cddc60deaccb6e18
Steve Laing <steve.laing@gmail.com>
2013-05-17 17:22:23 +0100

    Add required property to content fields

--- models/content.js
+++ models/content.js
@@ -3,9 +3,9 @@ var mongoose = require('mongoose');
 var Content = function() {
 
   var contentSchema = mongoose.Schema({
-    title: String,
-    path: String,
-    body: String
+    title: { type: String, required: true },
+    path: { type: String, required: true },
+    body: { type: String, required: true }
   }, { collection: 'contents' } );
 
   return mongoose.model('Content', contentSchema);
