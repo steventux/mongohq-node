a29c00a3c790d35bd455417a8a1dcd5839d82caf
Steve Laing <steve.laing@gmail.com>
2013-06-27 09:03:02 +0100

    Simplify Uer test

--- test/unit/user.js
+++ test/unit/user.js
@@ -1,19 +1,15 @@
 var helper          = require('../test-helper'),
     Factory         = helper.Factory,
-    passwordHash    = require('password-hash'),
-    User            = helper.User;
+    passwordHash    = require('password-hash');
 
 
 describe("User", function() {
   describe("validPassword", function(){
     it("should authenticate with a password", function(done) {
-        Factory.create("user", { password: passwordHash.generate("p455w0rd") }, function(){ 
-        User.findOne({ username: "admin" }, "username password", function(err, user) {
-          var testUser = new User(user);
-          testUser.validPassword("p455w0rd").should == true
-          testUser.validPassword("somethingElse").should == false
-        });
-        done(); 
+      Factory.create("user", { password: passwordHash.generate("p455w0rd") }, function(user){ 
+        user.validPassword("p455w0rd").should == true
+        user.validPassword("somethingElse").should == false
+        done();
       });
     });
   });
